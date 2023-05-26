c02112016a8245f98b3ed79a78aafc6bfbb3134d
GonzalezVictorr <89661414+GonzalezVictorr@users.noreply.github.com>
2023-05-26 20:33:43 -0300

    Do-While Acepta Sugerencia
    Se agregó en el main el do - while preguntando si se acepta o no la sugerencia

--- src/unlam/paradigmas/tp/hogwarts/Main.java
+++ src/unlam/paradigmas/tp/hogwarts/Main.java
@@ -1,9 +1,26 @@
 package unlam.paradigmas.tp.hogwarts;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Bienvenido a la Tierra Media");
-		System.out.println("Tierra Media le desea un lindo paseo!");
+		
+		Scanner entrada = new Scanner(System.in);
+		char respuesta;
+		
+		do {
+			System.out.println("¿Acepta sugerencia? Ingrese S o N: ");
+			respuesta = entrada.nextLine().charAt(0);
+			respuesta = Character.toUpperCase(respuesta); 
+			
+			
+		}while(respuesta != 'S' && respuesta !='N');
+		
+		if(respuesta == 'S')
+		System.out.println("La sugerencia ha sido aceptada!");
+		else
+			System.out.println("La sugerencia NO ha sido aceptada. :(");
 	}
+	
+	
 }
